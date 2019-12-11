fffe649c4ff6a2651f537b72089565188cbe1797
sa2922 <58683835+sa2922@users.noreply.github.com>
2019-12-11 09:11:16 +0100

    Add files via upload

---
+++ Tier.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+/**
+  *
+  * Beschreibung
+  *
+  * @version 1.0 vom 21.10.2019
+  * @author 
+  */
+
+public class Tier {
+  
+  // Anfang Attribute
+  // Ende Attribute
+  
+  // Anfang Methoden
+  // Ende Methoden
+} // end of Tier
+
