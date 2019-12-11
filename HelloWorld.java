79cd6fe29b501cc0e497befa8de30ef3611d4776
sa2922 <58683835+sa2922@users.noreply.github.com>
2019-12-11 09:10:13 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package Übung1;
+//Name der Klasse
+public class HelloWorld {
+
+	//Funktion/Methode mit dem {Methodenblock} (Parameter)
+	public static void main(String[] args) {
+		//
+		System.out.println("Hello World!");
+	
+	}
+	
+	public int gibZahl() {
+		
+		return 0;		
+	}
+	
+}
